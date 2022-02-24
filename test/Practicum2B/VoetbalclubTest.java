2e0172f18971eb00178ac4ffcf9d4abbb7af9652
Nick <nick.deheij@student.hu.nl>
2022-02-24 19:33:22 +0100

    Nieuw werk ingeleverd

--- test/Practicum2B/VoetbalclubTest.java
+++ test/Practicum2B/VoetbalclubTest.java
@@ -30,4 +30,53 @@ class VoetbalclubTest {
         assertEquals(0 , feij.aantalPunten(), "verloren resulteert in het verkeerd aantal punten");
     }
 
+    @Test
+    void testVerkeerdeInvoerTotaalPunten(){
+        feij.verwerkResultaat('r');
+       assertEquals(0,feij.aantalPunten(),"verkeerde invoer beinvloed het totaal aantal punten");
+    }
+
+    @Test
+    void testToStringResultaatVanW(){
+        feij.verwerkResultaat('w');
+        assertEquals("Feijenoord 1 1 0 0 3",feij.toString(),"punten worden niet goed in de toString gezet");
+
+    }
+
+    @Test
+    void testToStringResultaatVanG(){
+        feij.verwerkResultaat('g');
+        assertEquals("Feijenoord 1 0 1 0 1",feij.toString(),"punten worden niet goed in de toString gezet");
+
+    }
+
+    @Test
+    void testToStringResultaatVanV(){
+        feij.verwerkResultaat('v');
+        assertEquals("Feijenoord 1 0 0 1 0",feij.toString(),"punten worden niet goed in de toString gezet");
+
+    }
+
+    @Test
+    void testToStringResultaatVanWGV(){
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('g');
+        feij.verwerkResultaat('v');
+        assertEquals("Feijenoord 3 1 1 1 4",feij.toString(),"punten worden niet goed in de toString gezet");
+
+    }
+
+    @Test
+    void testHerhaaldelijkeInvoerGenereerdDeJuisteTostringResultaat(){
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('g');
+        feij.verwerkResultaat('v');
+        feij.verwerkResultaat('v');
+        feij.verwerkResultaat('g');
+        feij.verwerkResultaat('g');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        assertEquals("Feijenoord 8 3 3 2 12",feij.toString(),"punten worden niet goed in de toString gezet");
+
+    }
 }
