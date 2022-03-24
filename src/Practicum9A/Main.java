48d0b249b5c9f5c235d245683d9923ec510f3912
Nick <nick.deheij@student.hu.nl>
2022-03-24 16:18:48 +0100

    Nieuw werk ingeleverd

--- src/Practicum9A/Main.java
+++ src/Practicum9A/Main.java
@@ -3,7 +3,7 @@ package Practicum9A;
 public class Main {
     public static void main(String[] arg){
         double bedrag1 = 1123.5672245;
-        System.out.println("Zonder precisie:"+Utils.euroBedrag(bedrag1));
+        System.out.println("Zonder precisie:"+ Utils.euroBedrag(bedrag1));
         System.out.println("Met precisie:"+Utils.euroBedrag(bedrag1,1));
 
         double bedrag2 = 98769.5;
